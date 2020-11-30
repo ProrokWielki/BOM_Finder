d6b40c04616776d779cfb0c31413bc6c0e027aa6
ProrokWielki <ProrokWielki@o2.pl>
2020-11-30 20:46:05 +0100

    adde setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="BOMFinder",
+    version="0.0.1",
+    author="Paweł Warzecha",
+    author_email="ProrokWielki@o2.pl",
+    description="BOMFinder",
+    url="https://github.com/ProrokWielki/BOM_Finder",
+    packages=setuptools.find_packages()
+)
