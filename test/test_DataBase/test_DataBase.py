f1adc462c1963c340be0e2e44181ac6b2b60c52e
ProrokWielki <ProrokWielki@o2.pl>
2020-11-30 20:10:56 +0100

    added tests

--- test/test_DataBase/test_DataBase.py
+++ test/test_DataBase/test_DataBase.py
@@ -1,154 +1,152 @@
 import unittest
-from application.DataBase.DataBase import DataBase
- 
+
+from BOMFinder.DataBase.DataBase import DataBase
+
+
 class TestDataBase(unittest.TestCase):
     def setUp(self):
         self.data_base = DataBase()
- 
+
     def tearDown(self):
         unittest.TestCase.tearDown(self)
-        
+
     def test_add(self):
 
-        #Test integers
+        # Test integers
         self.data_base.add(1)
-        
+
         self.assertEqual(self.data_base.elements[0], 1)
         self.assertEqual(self.data_base.number_of_elements, 1)
-        
-        #Test string
+
+        # Test string
         self.data_base.add("a")
-        
+
         self.assertEqual(self.data_base.elements[1], "a")
         self.assertEqual(self.data_base.number_of_elements, 2)
-        
-        #Test dictionarry
+
+        # Test dictionarry
         test_dict = {"test": 2}
-        
+
         self.data_base.add(test_dict)
-        
+
         self.assertDictEqual(self.data_base.elements[2], test_dict)
         self.assertEqual(self.data_base.number_of_elements, 3)
-        
+
     def test_remove(self):
-        #Test integers
+        # Test integers
         self.data_base.add(1)
-        
-        
-        #Test string
+
+        # Test string
         self.data_base.add("a")
-                
-        #Test dictionarry
+
+        # Test dictionarry
         test_dict = {"test": 2}
-        
+
         self.data_base.add(test_dict)
-        
+
         self.data_base.remove("b")
         self.assertEqual(self.data_base.number_of_elements, 3)
-        
+
         self.data_base.remove("a")
         self.assertEqual(self.data_base.number_of_elements, 2)
-        
+
         self.data_base.remove(test_dict)
         self.assertEqual(self.data_base.number_of_elements, 1)
-        
-        self.data_base.remove(1)
-        self.assertEqual(self.data_base.number_of_elements, 0)
-        
+
         self.data_base.remove(1)
         self.assertEqual(self.data_base.number_of_elements, 0)
 
-            
-         
+        self.data_base.remove(1)
+        self.assertEqual(self.data_base.number_of_elements, 0)
+
     def test_get_number_of_elements(self):
-        
+
         self.assertEqual(self.data_base.get_number_of_elements(), 0)
-        
-        #Test integers
+
+        # Test integers
         self.data_base.add(1)
-        
+
         self.assertEqual(self.data_base.get_number_of_elements(), 1)
-        
-        #Test string
+
+        # Test string
         self.data_base.add("a")
-        
+
         self.assertEqual(self.data_base.get_number_of_elements(), 2)
-        
-        #Test dictionarry
+
+        # Test dictionarry
         test_dict = {"test": 2}
-        
+
         self.data_base.add(test_dict)
-        
+
         self.assertEqual(self.data_base.get_number_of_elements(), 3)
 
-
     def test_find_elements(self):
-        
+
         self.assertFalse(self.data_base.find_elements(1))
-        
-        #Test integers
+
+        # Test integers
         self.data_base.add(1)
-        
+
         self.assertTrue(self.data_base.find_elements(1))
-        
-        #Test string
+
+        # Test string
         self.data_base.add("a")
-        
+
         self.assertTrue(self.data_base.find_elements("a"))
-        
-        #Test dictionarry
+
+        # Test dictionarry
         test_dict = {"test": 2}
-        
+
         self.data_base.add(test_dict)
-        
+
         self.assertTrue(self.data_base.find_elements(test_dict))
-        
-        self.assertFalse(self.data_base.find_elements(7))      
-        
+
+        self.assertFalse(self.data_base.find_elements(7))
+
         self.data_base.add(1)
         self.assertEqual((self.data_base.find_elements(1)).get_elements(), [1, 1])
-        
-        
+
     def test_str(self):
-        
+
         self.assertFalse(self.data_base.find_elements(1))
-        
-        #Test integers
+
+        # Test integers
         self.data_base.add(1)
-        
+
         self.assertEqual(str(self.data_base), "1")
-        
-        #Test string
+
+        # Test string
         self.data_base.add("a")
-        
+
         self.assertEqual(str(self.data_base), "1\r\na")
-        
-        #Test dictionarry
+
+        # Test dictionarry
         test_dict = {"test": 2}
-        
+
         self.data_base.add(test_dict)
-        
+
         self.assertEqual(str(self.data_base), "1\r\na\r\n" + str(test_dict))
-        
+
     def test_get_elements(self):
-        
-        test_elements = [1 , "a", {"test": 2}]
-        
+
+        test_elements = [1, "a", {"test": 2}]
+
         self.data_base.add(test_elements[0])
-        
+
         self.assertEqual(self.data_base.get_elements(), test_elements[0:1])
-        
+
         self.data_base.add(test_elements[1])
-        
+
         self.assertEqual(self.data_base.get_elements(), test_elements[0:2])
-        
+
         self.data_base.add(test_elements[2])
-        
+
         self.assertEqual(self.data_base.get_elements(), test_elements)
-        
+
         self.data_base.remove(1)
-        
-        self.assertEqual(self.data_base.get_elements(), test_elements[1:3])       
-         
+
+        self.assertEqual(self.data_base.get_elements(), test_elements[1:3])
+
+
 if __name__ == '__main__':
     unittest.main()
--- test/test_Parts/test_Parts.py
+++ test/test_Parts/test_Parts.py
@@ -1,28 +1,37 @@
 import unittest
-from application.Parts.Parts import Value
- 
+
+from BOMFinder.Parts.Parts import Value, Part
+
+
+class DumyPart(Part):
+    properties = {}
+
+
 class TestValue(unittest.TestCase):
     def setUp(self):
-        pass    
-    
-    def tearDown(self):
-        #unittest.TestCase.tearDown(self)
         pass
-    
+
+    def tearDown(self):
+        # unittest.TestCase.tearDown(self)
+        pass
+
     def test_init(self):
-        Value(100)        
+        Value(100)
         Value(52.4)
         Value(0.123)
-        
+
         Value("100,0")
         Value("100.0")
-        
+
+        Value("0.1")
+        Value("0,1")
+
         Value("4k7")
 
         Value("0k47")
         Value("k47")
         Value("0.23k")
-        
+
         Value("1p")
         Value("1n")
         Value("1u")
@@ -32,45 +41,44 @@ class TestValue(unittest.TestCase):
         Value("1M")
         Value("1G")
         Value("1T")
-                        
+
         with self.assertRaises(ValueError):
             Value("100.0.0")
-            
+
         with self.assertRaises(ValueError):
             Value("100,0.0")
-            
+
         with self.assertRaises(ValueError):
             Value("100.G0")
-            
+
         with self.assertRaises(ValueError):
             Value("4k7k")
-            
+
         with self.assertRaises(ValueError):
             Value("1H")
-            
+
         with self.assertRaises(ValueError):
             Value("test")
-            
+
         with self.assertRaises(TypeError):
             Value([1, 2, 3])
-            
+
         with self.assertRaises(TypeError):
             Value({"test": 2})
-        
-        
+
     def test_eq(self):
         test_value_1 = Value(100)
         test_value_2 = Value("100")
- 
-        test_value_3 = Value(4.7)        
+
+        test_value_3 = Value(4.7)
         test_value_4 = Value("4.7")
         test_value_5 = Value("4,7")
-        
+
         test_value_6 = Value(4700)
         test_value_7 = Value("4k7")
         test_value_8 = Value("4.7k")
         test_value_9 = Value("4,7k")
-        
+
         test_value_10 = Value("1p")
         test_value_11 = Value("1n")
         test_value_12 = Value("1u")
@@ -80,19 +88,25 @@ class TestValue(unittest.TestCase):
         test_value_16 = Value("1M")
         test_value_17 = Value("1G")
         test_value_18 = Value("1T")
-        
+
+        test_value_19 = Value("100,0")
+        test_value_20 = Value("100.0")
+
+        test_value_21 = Value("0.1")
+        test_value_22 = Value("0,1")
+
         self.assertEqual(test_value_1, 100)
         self.assertEqual(test_value_2, test_value_1)
-        
+
         self.assertEqual(test_value_3, 4.7)
         self.assertEqual(test_value_4, test_value_3)
         self.assertEqual(test_value_4, test_value_5)
-        
+
         self.assertEqual(test_value_6, 4700)
         self.assertEqual(test_value_7, test_value_6)
         self.assertEqual(test_value_8, test_value_7)
         self.assertEqual(test_value_9, test_value_8)
-        
+
         self.assertEqual(test_value_10, 0.000000000001)
         self.assertEqual(test_value_11, 0.000000001)
         self.assertEqual(test_value_12, 0.000001)
@@ -102,14 +116,31 @@ class TestValue(unittest.TestCase):
         self.assertEqual(test_value_16, 1000000)
         self.assertEqual(test_value_17, 1000000000)
         self.assertEqual(test_value_18, 1000000000000)
-        
-    def test_get_value(self):  
+
+        self.assertEqual(test_value_19, 100)
+        self.assertEqual(test_value_20, 100)
+
+        self.assertEqual(test_value_21, 0.1)
+        self.assertEqual(test_value_22, 0.1)
+
+    def test_get_value(self):
         test_value_1 = Value(4700)
         self.assertEqual(test_value_1.get_value(), 4700)
-        
+
         test_value_2 = Value("4k7")
         self.assertEqual(test_value_2.get_value(), 4700)
-        
+
         test_value_3 = Value("4.7k")
         self.assertEqual(test_value_3.get_value(), 4700)
-        
+
+    def test_ammont_addition(self):
+        init_amount = 10
+        final_amount = 12
+
+        part = DumyPart(Amount=init_amount)
+
+        self.assertEqual(part.amount, init_amount)
+
+        part.amount = final_amount
+
+        self.assertEqual(part.amount, final_amount)
